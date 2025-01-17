6ef7c4b9bc6b3ab53490d825aad84ac50aca9cd2
Brandon Bybee <bybee.brandon1@gmail.com>
2025-01-16 21:00:07 -0600

    changed the nav file

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,63 +1,25 @@
 import { Link, useLocation } from "react-router-dom";
-
+import React from "react";
 
 function Nav() {
-  const currentPage = useLocation().pathname;
-
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
-      <div className="container-fluid">
-        <Link to="/" className="navbar-brand"></Link>
+    <nav>
+      <div>
+        <Link to="/"></Link>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link
-                to="/"
-                className={
-                  currentPage === "/"
-                    ? "nav-link active bg-primary-subtle"
-                    : "nav-link"
-                }
-              >
-            
-              </Link>
+        <div>
+          <ul>
+            <li>
+              <Link to="/"></Link>
             </li>
-            <li className="nav-item">
-              <Link
-                to="/Portfolio"
-                className={
-                  currentPage === "/Portfolio"
-                    ? "nav-link active bg-primary-subtle"
-                    : "nav-link"
-                }
-              >
-                Portfolio
-              </Link>
+            <li>
+              <Link to="/Portfolio">Portfolio</Link>
             </li>
-            <li className="nav-item">
-              <Link
-                to="/Contact"
-                className={
-                  currentPage === "/Contact"
-                    ? "nav-link active bg-primary-subtle"
-                    : "nav-link"
-                }
-              >
-                Contact
-              </Link>
+            <li>
+              <Link to="/Contact">Contact</Link>
             </li>
-            <li className="nav-item">
-              <Link
-                to="/Resume"
-                className={
-                  currentPage === "/Resume"
-                    ? "nav-link active bg-primary-subtle"
-                    : "nav-link"
-                }
-              >
-                Resume
-              </Link>
+            <li>
+              <Link to="/Resume">Resume</Link>
             </li>
           </ul>
         </div>
