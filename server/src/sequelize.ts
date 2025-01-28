f4f93a8eacecca4804db5ee59c62a20b5672f830
Brandon Bybee <bybee.brandon1@gmail.com>
2025-01-27 21:16:48 -0600

    added connection file

--- server/src/sequelize.ts
+++ server/src/sequelize.ts
@@ -3,15 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD!,
-  {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT!, 10),
-    dialect: 'postgres',
-  }
-);
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME!,
+    process.env.DB_USER!,
+    process.env.DB_PASSWORD!,
+    {
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!, 10),
+      dialect: 'postgres',
+    }
+  );
+}
 
 export default sequelize;
